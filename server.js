10a9a9a0704a02553d6c65b6d6e69347404cf28a
z20axa <alejandrospan@hotmail.com>
2023-01-15 21:13:08 -0500

    Adds import sequelize connection to server file

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
+
+// allow access to .env
+require('dotenv').config(); 
+
+
 // import sequelize connection
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SECRET, // gets secret from .env
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,5 +35,5 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  console.log(`App listening at http://localhost:${PORT}`);
 });
