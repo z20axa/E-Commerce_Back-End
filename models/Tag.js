f6acfa022fdb9e9ab30305c2d164de957099db78
z20axa <alejandrospan@hotmail.com>
2023-01-15 21:49:25 -0500

    Adds tag model requirements

--- models/Tag.js
+++ models/Tag.js
@@ -2,11 +2,26 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
     // define columns
+    // * `id`
+    id: {
+      // * Integer.
+      type: DataTypes.INTEGER,
+      // * Doesn't allow null values.
+      allowNull: false,
+      // * Set as primary key.
+      primaryKey: true,
+      // * Uses auto increment.
+      autoIncrement: true,
+    },
+    tag_name: {
+      // * string
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
