b9080724ecde58e45373c2bc35e82e25c768a217
z20axa <alejandrospan@hotmail.com>
2023-01-15 21:23:38 -0500

    Adds association methods to model files

--- models/index.js
+++ models/index.js
@@ -5,13 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: ''
+});
 
 // Categories have many Products
+Category.belongsToMany(Product, {
+  foreignKey: ''
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: ''
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: ''
+})
 
 module.exports = {
   Product,
