7fb2b8ec2854de51b806487cc62547e7cf321bde
z20axa <alejandrospan@hotmail.com>
2023-01-15 22:34:20 -0500

    Adds category route get route for one category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const oneCategory = await Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [Product]
+    })
+    res.status(200).json(oneCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
