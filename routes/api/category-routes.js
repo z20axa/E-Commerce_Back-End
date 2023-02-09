93819464353c2471e24b95f245f7991bd9f80e3e
z20axa <alejandrospan@hotmail.com>
2023-02-09 16:04:19 -0500

    Adds revisions code to the api category route CREATE, UPDATE, and DELETE methods

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,8 +10,10 @@ router.get('/', async (req, res) => {
   try {
     const allCategories = await Category.findAll({
     include: [Product]
-    })
+    });
+
     res.status(200).json(allCategories);
+
   } catch (err) {
     res.status(400).json(err);
   }
@@ -26,39 +28,60 @@ router.get('/:id', async (req, res) => {
         id: req.params.id
       },
       include: [Product]
-    })
+    });
+
     res.status(200).json(oneCategory);
+
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
 // CREATE a new category route
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
-  Category.create(req.body)
-  .then(data => res.json(data));
+  try {
+    const createCategory = await Category.create(req.body)
+
+    res.status(200).json(createCategory);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // UPDATE a new category by its id route
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  Category.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-  .then (data => res.json(data));
+  try {
+    const updateCategory = await Category.update(req.body, 
+      {
+      where: {
+        id: req.params.id,
+      },
+      });
+
+    res.status(200).json(updateCategory);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // DELETE a new category by its id route
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-  Category.destroy({
-    where: {
-      id: req.params.id,
-    }})
-  .then (data => res.json(data));
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }})
+
+    res.status(200).json(deleteCategory);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
