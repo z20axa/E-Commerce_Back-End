c215a30434d58aa0c40f848e0d5c79a0f4b92e75
z20axa <alejandrospan@hotmail.com>
2023-02-09 14:48:01 -0500

    Adds comments to routes index js file

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+// pacakges and module imports
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// /api route set up
 router.use('/api', apiRoutes);
 
+// wrong route warning
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
