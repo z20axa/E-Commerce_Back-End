657720bc7f2640d1506f6e07eb842355c57e42fe
z20axa <alejandrospan@hotmail.com>
2023-02-09 15:05:25 -0500

    Adds comments to the api tag route js file

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,24 +1,27 @@
+// pacakges and module imports
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// GET all tags route
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  // find all tags and include its associated Product data
 });
 
+// GET a single tag by id route
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // find a single tag by its `id` and include its associated Product data
 });
 
+// CREATE a new tag route
 router.post('/', (req, res) => {
   // create a new tag
   Tag.create(req.body)
     .then(data => res.json(data));
 });
 
+// UPDATE a tag by id route
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
   Tag.update(req.body, {
@@ -29,6 +32,7 @@ router.put('/:id', (req, res) => {
   .then (data => res.json(data));
 });
 
+// DELETE a tag by its id route
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
   Tag.destroy({
