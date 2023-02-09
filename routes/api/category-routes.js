50a952840c356e2ccc01d143be01468981e09ef7
z20axa <alejandrospan@hotmail.com>
2023-02-09 16:18:14 -0500

    Adds revisions code to the api category route CREATE, UPDATE, and DELETE methods

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,7 +41,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const createCategory = await Category.create(req.body)
+    const createCategory = await Category.create(req.body);
 
     res.status(200).json(createCategory);
 
