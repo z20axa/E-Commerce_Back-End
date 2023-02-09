e5d4497a2bd5d901ed75ae9d890b4139bffa9927
z20axa <alejandrospan@hotmail.com>
2023-02-09 14:45:18 -0500

    Adds comments to server js file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// packages/modules imports
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
@@ -5,18 +6,20 @@ const path = require('path');
 // allow access to .env
 require('dotenv').config(); 
 
-
 // import sequelize connection
 const sequelize = require('./config/connection');
 
+// initialize our app variable by setting it to the value of express()
 const app = express();
+
+// setting computer port number for service and Heruko deployment
 const PORT = process.env.PORT || 3001;
 
+// body parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(require('./controllers'));
-
+// routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
